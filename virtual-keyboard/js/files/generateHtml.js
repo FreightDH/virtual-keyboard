85431c8245a45fb50871fe05d8690774051b591b
FreightDH <maxim.zolotkov@yandex.ru>
2023-05-02 03:12:13 +0300

    fix: add key--input class

--- virtual-keyboard/js/files/generateHtml.js
+++ virtual-keyboard/js/files/generateHtml.js
@@ -1,4 +1,4 @@
-const labels = ['` ~', '1 !', '2 @', '3 #', '4 $', '5 %', '6 ^', '7 &', '8 *', '9 (', '0 )', '- _', '= +', '', 'del', 'tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[ {', '] }', '\\ |', 'EN', 'caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '; :', '\' "', '', '', 'shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ', &lt;', '. >', '/ ?', 'shift', '^', '', 'ctrl', 'option', 'cmd', '', 'cmd', 'ctrl', '^', '^', '^'];
+const labels = ['` ~', '1 !', '2 @', '3 #', '4 $', '5 %', '6 ^', '7 &', '8 *', '9 (', '0 )', '- _', '= +', '', 'del', 'tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[ {', '] }', '\\ |', 'EN', 'caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '; :', '\' "', '', '', 'shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ', <', '. >', '/ ?', 'shift', '^', '', 'ctrl', 'option', 'cmd', '', 'cmd', 'ctrl', '^', '^', '^'];
 const dataCodes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Delete', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', '', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', '', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ArrowUp', '', 'ControlLeft', 'AltLeft', 'MetaLeft', 'Space', 'MetaRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight']
 
 function generateHTML() {
@@ -8,7 +8,7 @@ function generateHTML() {
   textAreaInput.classList.add('textarea__input');
   textAreaInput.setAttribute('autofocus', '');
   textAreaWrapper.append(textAreaInput);
-  document.body.append(textAreaWrapper);
+  document.body.insertAdjacentElement('afterbegin', textAreaWrapper);
 
   const keyboard = document.createElement('div');
   keyboard.classList.add('keyboard');
@@ -21,28 +21,28 @@ function generateHTML() {
     key.setAttribute('data-code', dataCodes[i]);
     key.textContent = labels[i];
 
-    switch (i) {
-      case 13: key.classList.add('key--service'); key.classList.add('size-5'); break;
-      case 14: key.classList.add('key--service'); break;
-      case 15: key.classList.add('key--service'); key.classList.add('size-3'); break;
+    switch (true) {
+      case i === 13: key.classList.add('key--service'); key.classList.add('size-5'); break;
+      case i === 14: key.classList.add('key--service'); break;
+      case i === 15: key.classList.add('key--service'); key.classList.add('size-3'); break;
       case (i > 15 && i < 28): key.classList.add('key--input'); break;
-      case 28: key.classList.add('size-3'); break;
-      case 29: key.classList.add('key--service'); key.classList.add('change-language'); break;
-      case 30: key.classList.add('key--service'); key.classList.add('size-4'); break;
+      case i === 28: key.classList.add('size-3'); break;
+      case i === 29: key.classList.add('key--service'); key.classList.add('change-language'); break;
+      case i === 30: key.classList.add('key--service'); key.classList.add('size-4'); break;
       case (i > 30 && i < 42): key.classList.add('key--input'); break;
-      case 42: key.classList.add('key--service'); key.classList.add('size-6'); break;
-      case 43: key.classList.add('key--service'); key.classList.add('change-color'); break;
-      case 44: key.classList.add('key--service'); key.classList.add('size-6'); break;
+      case i === 42: key.classList.add('key--service'); key.classList.add('size-6'); break;
+      case i === 43: key.classList.add('key--service'); key.classList.add('change-color'); break;
+      case i === 44: key.classList.add('key--service'); key.classList.add('size-6'); break;
       case (i > 44 && i < 55): key.classList.add('key--input'); break;
-      case 55: key.classList.add('key--service'); key.classList.add('size-4'); break;
-      case 56: key.classList.add('key--service'); break;
-      case 57: key.classList.add('change-color'); break;
-      case 58: case 59: case 60: key.classList.add('key--service'); key.classList.add('size-2'); break;
-      case 61: key.classList.add('key--service'); key.classList.add('size-7'); break;
-      case 62: case 63: key.classList.add('key--service'); key.classList.add('size-3'); break;
-      case 64: key.classList.add('key--service'); key.classList.add('arrow-left'); break;
-      case 65: key.classList.add('key--service'); key.classList.add('arrow-down'); break;
-      case 66: key.classList.add('key--service'); key.classList.add('arrow-right'); break;
+      case i === 55: key.classList.add('key--service'); key.classList.add('size-4'); break;
+      case i === 56: key.classList.add('key--service'); break;
+      case i === 57: key.classList.add('change-color'); break;
+      case i === 58: case i === 59: case i === 60: key.classList.add('key--service'); key.classList.add('size-2'); break;
+      case i === 61: key.classList.add('key--service'); key.classList.add('size-7'); break;
+      case i === 62: case i === 63: key.classList.add('key--service'); key.classList.add('size-3'); break;
+      case i === 64: key.classList.add('key--service'); key.classList.add('arrow-left'); break;
+      case i === 65: key.classList.add('key--service'); key.classList.add('arrow-down'); break;
+      case i === 66: key.classList.add('key--service'); key.classList.add('arrow-right'); break;
     }
 
     keys.push(key);
@@ -86,7 +86,7 @@ function generateHTML() {
     keyboard.append(row);
   }
 
-  document.body.append(keyboard);
+  document.body.insertAdjacentElement('beforeend', keyboard);
 }
 
 generateHTML();
